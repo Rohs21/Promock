3984a65607fa9ae7c49b2b673af556de2a9d3cb9
Rohan Singh <rohans212004@gmail.com>
2025-09-06 14:40:44 +0530

    Clerk Auth + Header frontend

--- app/page.js
+++ app/page.js
@@ -1,9 +1,14 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+
 
 export default function Home() {
   return (
-    <div>
-      Hi there
+    <div className="">
+      this is the pro-mock Landing page
+      <Button>
+        Submit
+      </Button>
     </div>
   );
 }
